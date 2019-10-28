6fcf35f1e0dfcabe0b4e5746e22f9d28ffc9c181
CallumGrindle <callumgrindle@gmail.com>
2019-10-28 17:49:53 +0000

    added component structure

---
+++ src/components/Film.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Film extends Component {
+  render() {
+    return <h1>I am film</h1> 
+  }
+}
+
+export default Film;
---
+++ src/components/FilmList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Film from './Film.js'
+
+class FilmList extends Component {
+  render() {
+    return (
+      <Film />
+    )
+  }
+}
+
+export default FilmList;
